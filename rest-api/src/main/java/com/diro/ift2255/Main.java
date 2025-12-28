085a68455f4be64532f972d9780aeb0ec891ca34
Thomas-Ring <ringthomas4321@gmail.com>
2025-12-28 15:44:05 -0500

    Ajouté recherche de cours par sigle très basique

--- rest-api/src/main/java/com/diro/ift2255/Main.java
+++ rest-api/src/main/java/com/diro/ift2255/Main.java
@@ -1,13 +1,20 @@
-package com.diro.ift2255.cli;
+package com.diro.ift2255;
 
+import com.diro.ift2255.model.Course;
 import com.diro.ift2255.serveur.DemarreurServeur;
+import com.diro.ift2255.service.CourseService;
+import com.diro.ift2255.util.*;
 import io.javalin.Javalin;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        HttpClientApi clientApi = new HttpClientApi();
+        CourseService courseService = new CourseService(clientApi);
 
         System.out.println(System.lineSeparator() + "Bienvenue à ChoixCours: un logiciel vous permettant d'éclairer vos choix de cours universitaires." + System.lineSeparator());
         System.out.println("Commandes disponibles:");
@@ -31,6 +38,20 @@ public class Main {
                 case "demarrerServeur":
                     app = DemarreurServeur.main();
                     break;
+
+                case "rechercherCours":
+                    System.out.println("Veuillez entrer un sigle de cours.");
+                    String sigle = scanner.nextLine().trim();
+
+                    Optional<Course> optionalCourse = courseService.getCourseById(sigle);
+
+                    if (optionalCourse.isPresent()) {
+                        Course course = optionalCourse.get();
+                        System.out.println("Cours trouvé: " + course.getName() + " (" + course.getId() + ")");
+                        System.out.println("Description: " + course.getDescription());
+                    }
+
+
             }
         }
 
